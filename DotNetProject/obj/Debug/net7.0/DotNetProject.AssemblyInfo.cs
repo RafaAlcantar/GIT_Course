83dc24048b073e2662caddd847e8ee15179b4a21
Rafa Alcantar <rafa.cia.aj.11@gmail.com>
2025-05-21 14:12:07 -0600

    a

--- DotNetProject/obj/Debug/net7.0/DotNetProject.AssemblyInfo.cs
+++ DotNetProject/obj/Debug/net7.0/DotNetProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6eba3c21b9b74fcf0f03cf22ad63722444cfb5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413176a5c64cbfafccadbe88169062308917ad99")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
